b1c89376d25b5cad68a5d4fdd67ececbe4c56464
serg <serkhas@hotmail.com>
2025-09-28 00:41:54 -0700

    Add extraction script

---
+++ extract.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This extracts the ffmpeg/ffprobe binaries from the built image
+mkdir -p bin
+docker run --rm -v $(pwd)/bin:/output ffmpeg7-audio-al2023:latest cp /usr/local/bin/ffmpeg /usr/local/bin/ffprobe /output/
+
+# Now we can build the zip for the layer
+zip -r9 ffmpeg7-audio-al2023.zip bin
